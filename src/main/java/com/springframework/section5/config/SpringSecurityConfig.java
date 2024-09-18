85c918eb4d78478784217d10bcf218ff59a5f6da
Lidija Silantjeva <dalilija@gmail.com>
2024-09-18 18:31:16 +0300

    OpenAPI

--- src/main/java/com/springframework/section5/config/SpringSecurityConfig.java
+++ src/main/java/com/springframework/section5/config/SpringSecurityConfig.java
@@ -2,10 +2,12 @@ package com.springframework.section5.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Profile("!test")
 @Configuration
 public class SpringSecurityConfig {
 
@@ -13,7 +15,9 @@ public class SpringSecurityConfig {
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		http.authorizeHttpRequests(
-				auth -> auth.anyRequest().authenticated()
+				auth -> auth
+					.requestMatchers("/v3/api-docs**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+					.anyRequest().authenticated()
 			)
 			.oauth2ResourceServer(
 				httpSecurityOAuth2ResourceServerConfigurer ->
